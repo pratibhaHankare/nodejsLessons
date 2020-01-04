ac15d4cb7c40be48a3e8cce246d3a88ea2f23e35
pratibha hankare <pratibha1hankare@gmail.com>
2020-01-04 14:18:39 +0530

    the process how node stores the data in file and how it processes the message

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{ // creating server
-  //  console.log(req);
-  //  console.log(req.url,req.method,req.headers);
    // process.exit(); // used to exit the process
+
+   /**Raw logic how node works with forms */
+    const url = req.url;
+    const method = req.method;
+    if(url ==='/'){
+        res.write(`<html><head><title>Meassge sender</title></head><body><h2>Message sender</h2><form method="POST" action="/message"><input type="text" name="message"><button type="submit">Send</button></button></form></body></html>`);
+        return res.end()
+    }
+    // have to be recevie
+    if(url === '/message' && method === 'POST'){
+        const body =[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('meassge.txt',message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
    //setting response
    res.setHeader('Content-type','text/html');
    res.write(`<html><head><title>My first server response</title></head>`);
